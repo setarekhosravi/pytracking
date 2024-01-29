1f79a4c44d8a8dfb74da48afc514b9d8859807d8
Setare Khosravi <strhkhosravi@gmail.com>
2024-01-29 15:25:26 +0330

    modify run_video.py

--- pytracking/run_video.py
+++ pytracking/run_video.py
@@ -9,7 +9,7 @@ if env_path not in sys.path:
 from pytracking.evaluation import Tracker
 
 
-def run_video(tracker_name, tracker_param, videofile, optional_box=None, debug=None, save_results=False):
+def run_video(tracker_name, tracker_param, videofile, optional_box=None, debug=None, save_results=True, gt_path=None, update_rate=None):
     """Run the tracker on your webcam.
     args:
         tracker_name: Name of tracking method.
@@ -17,7 +17,7 @@ def run_video(tracker_name, tracker_param, videofile, optional_box=None, debug=N
         debug: Debug level.
     """
     tracker = Tracker(tracker_name, tracker_param)
-    tracker.run_video_generic(videofilepath=videofile, optional_box=optional_box, debug=debug, save_results=save_results)
+    tracker.run_video_generic(videofilepath=videofile, optional_box=optional_box, debug=debug, save_results=save_results, gt_path=gt_path, update_rate=update_rate)
 
 def main():
     parser = argparse.ArgumentParser(description='Run the tracker on your webcam.')
@@ -25,13 +25,15 @@ def main():
     parser.add_argument('tracker_param', type=str, help='Name of parameter file.')
     parser.add_argument('videofile', type=str, help='path to a video file.')
     parser.add_argument('--optional_box', type=float, default=None, nargs="+", help='optional_box with format x y w h.')
+    parser.add_argument('--gt_path', type=str, default=None, help='ground truth directory')
+    parser.add_argument('--update_rate', type=int, default=30, help='updating per specified frames from ground truth')
     parser.add_argument('--debug', type=int, default=0, help='Debug level.')
     parser.add_argument('--save_results', dest='save_results', action='store_true', help='Save bounding boxes')
-    parser.set_defaults(save_results=False)
+    parser.set_defaults(save_results=True)
 
     args = parser.parse_args()
 
-    run_video(args.tracker_name, args.tracker_param,args.videofile, args.optional_box, args.debug, args.save_results)
+    run_video(args.tracker_name, args.tracker_param,args.videofile, args.optional_box, args.debug, args.save_results, args.gt_path, args.update_rate)
 
 
 if __name__ == '__main__':
